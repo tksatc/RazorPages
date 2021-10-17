c25b37ea153f1ca14c846ad99564adfeb9c96217
tksatc <tksatc@gmail.com>
2021-10-17 14:06:53 -0800

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RazorPages.Models;
+
+namespace RazorPages.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SpaContext _context;
+
+        public HomeController(SpaContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult InquiryForm()
+        {
+            ViewBag.Posted = false;
+            ViewData["ServicesID"] = new SelectList(_context.Services, "Classification", "Classification");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult InquiryForm(Contact model)
+        {
+            ViewBag.Posted = true;
+            return View(model);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Employees()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Migrations/20201003191803_initialCreate.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPages.Migrations
+{
+    public partial class initialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Services",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Classification = table.Column<string>(nullable: true),
+                    Fee = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Services", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    ServicesID = table.Column<int>(nullable: false),
+                    ContactEmail = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Contacts_Services_ServicesID",
+                        column: x => x.ServicesID,
+                        principalTable: "Services",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Services",
+                columns: new[] { "ID", "Classification", "Fee", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Full", 450.0, "Full-Day Treatment Package" },
+                    { 2, "Half", 300.0, "Half-Day Treatment Package" },
+                    { 3, "Two", 225.0, "Two-Hour Treatment Package" },
+                    { 4, "One", 100.0, "One-Hour Treatment Package" },
+                    { 5, "Other", 200.0, "Custom Treatment Package" },
+                    { 6, "Salon", 45.0, "Haircut or Trim" },
+                    { 7, "Salon", 100.0, "Full Foil Color" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "ID", "ContactEmail", "Name", "ServicesID" },
+                values: new object[] { 1, "wilma@flint.net", "Wilma Flintstone", 1 });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "ID", "ContactEmail", "Name", "ServicesID" },
+                values: new object[] { 3, "betts@rubb.com", "Betty Rubble", 5 });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "ID", "ContactEmail", "Name", "ServicesID" },
+                values: new object[] { 2, "Barn@rubb.com", "Barney Rubble", 7 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_ServicesID",
+                table: "Contacts",
+                column: "ServicesID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+
+            migrationBuilder.DropTable(
+                name: "Services");
+        }
+    }
+}
